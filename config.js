89b882db12c92774fd59adc1326f4fe3bb44986e
jin1963 <kaojino89@gmail.com>
2025-07-22 15:49:13 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-
-const contractAddress = "0xcF66b9491F15fD415241D8B717f1F5673580B1dD";
-const tokenAddress = "0xd479ae350dc24168e8db863c5413c35fb2044ecd";
-const rewardWallet = "0x62271BA79E2809091CF393eC1F313123271cEe2B";
-const chainId = 56;
